cddb99c03f264282b2db323c19366558183653d3
John <jjpyae@gmail.com>
2023-05-08 16:37:02 -0700

    All tests passed

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,7 +1,29 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+  static combinedYearsOfExperience(teachers){
+    //Must be an array
+    if (!Array.isArray(teachers)) {
+      throw Error('combinedYearsOfExperience only takes the array as an argument');
+    }
 
+    //All must be instances of Teacher
+    for (let teacher of teachers) {
+      if (!(teacher instanceof Teacher)) {
+        throw Error('All items in the array must be the instances of Teacher');
+      }
+    }
+
+    //Will get here if the above conditions are true
+    return teachers.reduce((sum, teacher) => sum + teacher.yearsOfExperience, 0);
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
